f4e37456bdff61d9e5f1076f6ccd4a1edbb4ca6a
freddie41 <fcl41@me.com>
2021-10-15 13:36:49 -0300

    Se agregan funcionalidades para mostrar datos y calculos del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,173 +1,237 @@
-var cartPoruducts = {};
+//Variable para almancenar el arreglo obtenido al llamar al endpoint
+var cartPoruducts = [];
 
-// Controles para sumar/restar cantidad de articulos agregados al carrito
+//Valor de la cotización del USD
+var conversionValueUSD = 40;
+
+//Controles para sumar/restar cantidad de articulos agregados al carrito
 function itemCountControls() {
   
-  $('.btn-number').click(function(e){
+  $('.btn-number').click(function (event) {
 
-    e.preventDefault();
-    
+    event.preventDefault();
+
     fieldName = $(this).attr('data-field');
-    type      = $(this).attr('data-type');
-    var input = $("input[name='"+fieldName+"']");
+    type = $(this).attr('data-type');
+    var input = $("input[name='" + fieldName + "']");
     var currentVal = parseInt(input.val());
+
     if (!isNaN(currentVal)) {
-        if(type == 'minus') {
-            
-            if(currentVal > input.attr('min')) {
-                input.val(currentVal - 1).change();
-            } 
-            if(parseInt(input.val()) == input.attr('min')) {
-                $(this).attr('disabled', true);
-            }
+      if (type == 'minus') {
 
-        } else if(type == 'plus') {
-
-            if(currentVal < input.attr('max')) {
-                input.val(currentVal + 1).change();
-            }
-            if(parseInt(input.val()) == input.attr('max')) {
-                $(this).attr('disabled', true);
-            }
-
+        if (currentVal > input.attr('min')) {
+          input.val(currentVal - 1).change();
         }
+        // Aca es donde no se esta aplicando correctamente la regla al momento de cargar la pagina del carrito
+        // en el primer if entiendo que esta controlando que si el valor es > 1 se quita el atributo para desactivar el botón y
+        //al cargar la pagina en el articulo del pino deberia mantener el atributo ya que comienza con value = 2.
+        if (parseInt(input.val()) == input.attr('min')) {
+          $(this).attr('disabled', true);
+        }
+
+      } else if (type == 'plus') {
+
+        if (currentVal < input.attr('max')) {
+          input.val(currentVal + 1).change();
+        }
+        if (parseInt(input.val()) == input.attr('max')) {
+          $(this).attr('disabled', true);
+        }
+      }
     } else {
-        input.val(0);
+      input.val(0);
     }
-});
+  });
 
-$('.input-number').focusin(function(){
-   $(this).data('oldValue', $(this).val());
-});
+  $('.input-number').focusin(function () {
+    $(this).data('oldValue', $(this).val());
+  });
 
-$('.input-number').change(function() {
-    
-    minValue =  parseInt($(this).attr('min'));
-    maxValue =  parseInt($(this).attr('max'));
+  $('.input-number').change(function () {
+
+    minValue = parseInt($(this).attr('min'));
+    maxValue = parseInt($(this).attr('max'));
     valueCurrent = parseInt($(this).val());
-    
-    let name = $(this).attr('name');
-    if(valueCurrent >= minValue) {
-        $(".btn-number[data-type='minus'][data-field='"+ name +"']").removeAttr('disabled')
-    } else {
-        $(this).val($(this).data('oldValue'));
-    }
-    if(valueCurrent <= maxValue) {
-        $(".btn-number[data-type='plus'][data-field='"+ name +"']").removeAttr('disabled')
-    } else {
-        $(this).val($(this).data('oldValue'));
-    }
-});
 
-$(".input-number").keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-             // Allow: Ctrl+A
-            (e.keyCode == 65 && e.ctrlKey === true) || 
-             // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
-    });
+    let name = $(this).attr('name');
+
+    if (valueCurrent >= minValue) {
+      $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+    } else {
+      $(this).val($(this).data('oldValue'));
+    }
+    if (valueCurrent <= maxValue) {
+      $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+    } else {
+      $(this).val($(this).data('oldValue'));
+    }
+  });
 }
 
-// Funcion para mostrar el listado de productos del carrito
+//Funcion para calcular el total del carrito
+function calcTotal() {
+
+  let totalCarrito = 0;
+  let subtotalsArray = document.getElementsByClassName("subtotal");
+
+  for (let i = 0; i < subtotalsArray.length; i++) {
+
+    totalCarrito += parseInt(subtotalsArray[i].innerHTML);
+    
+    document.getElementById("subtotalCarrito").innerHTML = totalCarrito;
+    document.getElementById("totalCarrito").innerHTML = totalCarrito;
+  }
+}
+
+//Funcion para recalcular el subtotal en tiempo real al modificar cantidades
+function calcSubTotal(unitCost, index) {
+  
+  let count = parseInt(document.getElementById(`count${index}`).value);
+  let currencyConv = document.getElementById(`artCurrency${index}`);
+
+  //Control para hacer la conversion de moneda de UYU a USD
+  if (currencyConv.innerText === "UYU") {
+    subtotal = (unitCost * count) / conversionValueUSD;
+  } else {
+    subtotal = unitCost * count;
+  }
+
+  document.getElementById(`artSubtotal${index}`).innerHTML = subtotal;
+  document.getElementById(`artSummarySubTotal${index}`).innerHTML = subtotal;
+
+  calcTotal();
+}
+
+//Funcion para mostrar el listado de productos del carrito
 function showCartProducts(array) {
 
     let htmlContentToAppend = "";
 
-    for (let item of array) {
+    for (let i = 0; i < array.length; i++) {
 
-        let article = item;
+        let article = array[i];
+        let currencyConv = article.currency;
+        let artSubTotal = 0;
+
+        //Control para hacer la conversion de moneda de UYU a USD
+        if (currencyConv === "UYU") {
+          artSubTotal = (article.unitCost * article.count) / conversionValueUSD;
+        } else {
+          artSubTotal = article.unitCost * article.count;
+        }
 
         htmlContentToAppend += `
         <!-- Cart item -->
         <div class="row">
-          <div class="col-md-5 col-lg-3">
+          <div class="col-12 col-md-4">
             <div class="d-block h-100">
               <img class="img-fluid img-thumbnail mb-3"
                 src="${article.src}" alt="articleImg">
             </div>
           </div>
-          <div class="col-md-7 col-lg-9">
+          <div class="col-12 col-md-8">
             <div>
-              <div class="d-flex justify-content-between">
-                <div>
-                  <h5>${article.name}</h5>
-                  <p class="my-3 text-muted text-uppercase small">${article.currency + " " + article.unitCost}</p>
-                </div>
-
-                <!-- Item unit controls -->
-                <div>
-                  <div class="def-number-input number-input safari_only mb-0 w-100">
-                    <div class="input-group">
-                      <span class="input-group-prepend">
-                        <button type="button" class="btn btn-outline-info btn-number" disabled="disabled" data-type="minus"
-                          data-field="quant[1]">
-                          <span class="fa fa-minus"></span>
-                        </button>
-                      </span>
-                      <input type="number" name="quant[1]" class="form-control input-number text-center" value="1" min="1" max="10">
-                      <span class="input-group-append">
-                        <button type="button" class="btn btn-outline-info btn-number" data-type="plus" data-field="quant[1]">
-                          <span class="fa fa-plus"></span>
-                        </button>
-                      </span>
+              <div class="">
+                <div class="row d-flex justify-content-between">
+                  <div class="col-6 col-md-8 pt-2">
+                    <h5>${article.name}</h5>
+                  </div>
+                  <div class="col-6 col-md-4">
+                    <!-- Item unit controls -->
+                    <div class="def-number-input number-input mb-0 w-100">
+                      <div class="input-group">
+                        <span class="input-group-prepend">
+                          <button type="button" class="btn btn-outline-info btn-number" disabled="disabled" data-type="minus"
+                            data-field="control${i}">
+                            <span class="fa fa-minus"></span>
+                          </button>
+                        </span>
+                        <input type="number" id="count${i}" name="control${i}" class="form-control input-number text-center"
+                          onchange="calcSubTotal(${article.unitCost}, ${i});" value="${article.count}" min="1" max="5">
+                        <span class="input-group-append">
+                          <button type="button" class="btn btn-outline-info btn-number" data-type="plus" data-field="control${i}">
+                            <span class="fa fa-plus"></span>
+                          </button>
+                        </span>
+                      </div>
                     </div>
+                    <!-- End item unit controls -->
                   </div>
                 </div>
-                <!-- Item unit controls -->
-
-              </div>
-              <div class="d-flex justify-content-between align-items-center">
-                <div>
-                  <a type="button" class="text-danger small text-uppercase mr-3">
-                    <i class="fas fa-trash-alt mr-1"></i>Borrar</a>
+                <div class="row d-flex justify-content-between align-items-center">
+                  <div class="col-12 col-md-6">
+                    <p class="text-muted my-1">
+                      <span class="mr-1 small">Costo:</span>
+                      <span class="font-weight-bold small" id="artCurrency${i}">${article.currency}</span>
+                      <span class="font-weight-bold small">${article.unitCost}</span>
+                    </p>
+                  </div>
                 </div>
-                <p class="mb-0"><span><strong>${article.currency + " " + article.unitCost}</strong></span></p>
+                <div class="row d-flex justify-content-between align-items-center">
+                  <div class="col-5 col-lg-6 mt-4">
+                    <a type="button" class="text-danger small text-uppercase">
+                      <i class="fas fa-trash-alt mr-1"></i>Borrar
+                    </a>
+                  </div>
+                  <div class="col-7 col-lg-6 mt-4 text-right">
+                    <span class="small text-muted pr-1">Costo Total:</span>
+                    <span class="small font-weight-bold px-0">USD</span>
+                    <span class="subtotal font-weight-bold" id="artSubtotal${i}">${artSubTotal}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <!-- Cart item -->
+        <!-- End cart item -->
         
         <hr class="mb-4">
         
         `
-
         document.getElementById("cart-items").innerHTML = htmlContentToAppend;
+
+        //Aca es donde entiendo que debera de poder calcularse el subtotal con el control de divisa 
+        //modificando el valor anterior al momento de cargar la pagina
+        //pero se me genera un error que no me doy cuenta cual es
+        
+        ////* calcSubTotal(article.unitCost, i); *///
     }
 }
 
-// Funcion para mostrar los articulos en el resumen de pedido
+//Funcion para mostrar los articulos en el resumen de pedido
 function showSummaryProducts(array) {
   
   let htmlContentToAppend = "";
 
-    for (let item of array) {
+    for (let i = 0; i < array.length; i++) {
 
-        let article = item;
+        let article = array[i];
+        let currencyConv = article.currency;
+        let artSubTotal = 0;
+
+        //Control para hacer la conversion de moneda de UYU a USD
+        if (currencyConv === "UYU") {
+          artSubTotal = (article.unitCost * article.count) / conversionValueUSD;
+        } else {
+          artSubTotal = article.unitCost * article.count;
+        }
 
         htmlContentToAppend += `
-        <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 py-1">
-          <small>${article.name}</small>
-          <span><small>${article.currency + " " + article.unitCost}</small></span>
+        <li class="list-group-item font-weight-lighter d-flex justify-content-between align-items-center border-0 px-0 py-1">
+          <div>${article.name}</div>
+          <div class="d-flex align-items-center">
+            <div class="small mr-1">USD</div>
+            <div id="artSummarySubTotal${i}">${artSubTotal}</div>
+          </div>
         </li>
         `
-
         document.getElementById("summaryItems").innerHTML = htmlContentToAppend;
     }
 }
 
 // Funcion para contar la cantidad de articulos agregados al carrito
 function showProductsCount(array) {
-
-        document.getElementById("productCount").innerText = array.length;
+  document.getElementById("productCount").innerText = array.length;
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -179,29 +243,32 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     if (resultObj.status === "ok") {
 
-      cartPoruducts = resultObj.data
+      cartPoruducts = resultObj.data.articles;
 
       // Muestro el conteo de productos agregados al carrito
-      showProductsCount(cartPoruducts.articles);
+      showProductsCount(cartPoruducts);
 
       // Muestro la info de los productos agregados al carrito
-      showCartProducts(cartPoruducts.articles);
+      showCartProducts(cartPoruducts);
 
-      // Muestro los controles de cantidad
+      //Muestro los controles de cantidad
       itemCountControls();
 
       // Muestro los productos del carrito en el resumen de pedido
-      showSummaryProducts(cartPoruducts.articles);
+      showSummaryProducts(cartPoruducts);
+
+      //Muestro los valores totales del carrito
+      calcTotal();
     }
   });
 
   // Inicializador de tooltips
-  $(document).ready(function() {
+  $(this).ready(function() {
       $("[data-toggle=tooltip]").tooltip();
   });
 
   // Inicializador de popovers
-  $(document).ready(function(){
+  $(this).ready(function(){
       $('[data-toggle="popover"]').popover();
   });    
 });
