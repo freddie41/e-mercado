9fabf4ab1a90e8c83b9cc7824e1d953cf3aaa64f
freddie41 <fcl41@me.com>
2021-10-17 22:56:03 -0300

    Se corrigen funciones de cambio de cantidades y calculos del carrito

--- js/cart.js
+++ js/cart.js
@@ -11,31 +11,22 @@ function itemCountControls() {
 
     event.preventDefault();
 
-    fieldName = $(this).attr('data-field');
+    fieldId = $(this).attr('data-id');
     type = $(this).attr('data-type');
-    var input = $("input[name='" + fieldName + "']");
+    var input = $(`#${fieldId}`);
     var currentVal = parseInt(input.val());
 
-    if (!isNaN(currentVal)) {
+    if (currentVal) {
       if (type == 'minus') {
 
         if (currentVal > input.attr('min')) {
           input.val(currentVal - 1).change();
-        }
-        // Aca es donde no se esta aplicando correctamente la regla al momento de cargar la pagina del carrito
-        // en el primer if entiendo que esta controlando que si el valor es > 1 se quita el atributo para desactivar el botón y
-        //al cargar la pagina en el articulo del pino deberia mantener el atributo ya que comienza con value = 2.
-        if (parseInt(input.val()) == input.attr('min')) {
-          $(this).attr('disabled', true);
         }
 
       } else if (type == 'plus') {
 
         if (currentVal < input.attr('max')) {
           input.val(currentVal + 1).change();
-        }
-        if (parseInt(input.val()) == input.attr('max')) {
-          $(this).attr('disabled', true);
         }
       }
     } else {
@@ -47,24 +38,29 @@ function itemCountControls() {
     $(this).data('oldValue', $(this).val());
   });
 
-  $('.input-number').change(function () {
+  $('.input-number').change(function (event, flag) {
 
     minValue = parseInt($(this).attr('min'));
     maxValue = parseInt($(this).attr('max'));
     valueCurrent = parseInt($(this).val());
 
-    let name = $(this).attr('name');
+    let countId = $(this).attr('id');
 
-    if (valueCurrent >= minValue) {
-      $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+    if (valueCurrent > minValue) {
+      $(".btn-number[data-type='minus'][data-id='" + countId + "']").prop('disabled', false);
     } else {
-      $(this).val($(this).data('oldValue'));
+      $(".btn-number[data-type='minus'][data-id='" + countId + "']").prop('disabled', true);
     }
-    if (valueCurrent <= maxValue) {
-      $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+    if (valueCurrent < maxValue) {
+      $(".btn-number[data-type='plus'][data-id='" + countId + "']").prop('disabled', false);
     } else {
-      $(this).val($(this).data('oldValue'));
+      $(".btn-number[data-type='plus'][data-id='" + countId + "']").attr('disabled', true);
     }
+
+    if (flag !== "not_subtotal") {
+      calcSubTotal();  
+    }
+
   });
 }
 
@@ -72,32 +68,44 @@ function itemCountControls() {
 function calcTotal() {
 
   let totalCarrito = 0;
-  let subtotalsArray = document.getElementsByClassName("subtotal");
 
-  for (let i = 0; i < subtotalsArray.length; i++) {
+  totalCarrito = $("#subtotalCarrito").text();
+  
+  //Costo de envio se agrega AQUI para sumar al total del carrito
 
-    totalCarrito += parseInt(subtotalsArray[i].innerHTML);
-    
-    document.getElementById("subtotalCarrito").innerHTML = totalCarrito;
-    document.getElementById("totalCarrito").innerHTML = totalCarrito;
-  }
+  $("#totalCarrito").html(totalCarrito);
 }
 
 //Funcion para recalcular el subtotal en tiempo real al modificar cantidades
-function calcSubTotal(unitCost, index) {
-  
-  let count = parseInt(document.getElementById(`count${index}`).value);
-  let currencyConv = document.getElementById(`artCurrency${index}`);
+function calcSubTotal() {
 
-  //Control para hacer la conversion de moneda de UYU a USD
-  if (currencyConv.innerText === "UYU") {
-    subtotal = (unitCost * count) / conversionValueUSD;
-  } else {
-    subtotal = unitCost * count;
+  let counts = $("[data-count]");
+
+  let subTotals = 0;
+
+  let totalCarrito = 0;
+
+  for (let i = 0; i < counts.length; i++) {
+
+    let $item = $(counts[i]);
+    let countIndex = $item.attr('data-count');
+    let currencyConv = $item.attr('data-currency');
+    let count = $item.val();
+    let unitCost = $item.attr('data-unitcost');
+
+    //Control para hacer la conversion de moneda de UYU a USD
+    if (currencyConv === "UYU") {
+      subTotals = (unitCost * count) / conversionValueUSD;
+    } else {
+      subTotals = unitCost * count;
+    }
+
+    $(`#artSubtotal${countIndex}`).html(subTotals);
+    $(`#artSummarySubTotal${countIndex}`).html(subTotals);
+
+    totalCarrito += subTotals;
   }
-
-  document.getElementById(`artSubtotal${index}`).innerHTML = subtotal;
-  document.getElementById(`artSummarySubTotal${index}`).innerHTML = subtotal;
+  $("#subtotalCarrito").html(totalCarrito);
 
   calcTotal();
 }
@@ -105,7 +113,7 @@ function calcSubTotal(unitCost, index) {
 //Funcion para mostrar el listado de productos del carrito
 function showCartProducts(array) {
 
-    let htmlContentToAppend = "";
+    let htmlContent = "";
 
     for (let i = 0; i < array.length; i++) {
 
@@ -120,7 +128,7 @@ function showCartProducts(array) {
           artSubTotal = article.unitCost * article.count;
         }
 
-        htmlContentToAppend += `
+        htmlContent += `
         <!-- Cart item -->
         <div class="row">
           <div class="col-12 col-md-4">
@@ -141,15 +149,15 @@ function showCartProducts(array) {
                     <div class="def-number-input number-input mb-0 w-100">
                       <div class="input-group">
                         <span class="input-group-prepend">
-                          <button type="button" class="btn btn-outline-info btn-number" disabled="disabled" data-type="minus"
-                            data-field="control${i}">
+                          <button type="button" class="btn btn-outline-info btn-number" data-type="minus"
+                            data-id="count${i}">
                             <span class="fa fa-minus"></span>
                           </button>
                         </span>
-                        <input type="number" id="count${i}" name="control${i}" class="form-control input-number text-center"
-                          onchange="calcSubTotal(${article.unitCost}, ${i});" value="${article.count}" min="1" max="5">
+                        <input id="count${i}" name="control${i}" class="form-control input-number text-center"
+                        value="${article.count}" min="1" max="5" data-count="${i}" data-currency="${article.currency}" data-unitcost="${article.unitCost}">
                         <span class="input-group-append">
-                          <button type="button" class="btn btn-outline-info btn-number" data-type="plus" data-field="control${i}">
+                          <button type="button" class="btn btn-outline-info btn-number" data-type="plus" data-id="count${i}">
                             <span class="fa fa-plus"></span>
                           </button>
                         </span>
@@ -162,7 +170,7 @@ function showCartProducts(array) {
                   <div class="col-12 col-md-6">
                     <p class="text-muted my-1">
                       <span class="mr-1 small">Costo:</span>
-                      <span class="font-weight-bold small" id="artCurrency${i}">${article.currency}</span>
+                      <span class="font-weight-bold small">${article.currency}</span>
                       <span class="font-weight-bold small">${article.unitCost}</span>
                     </p>
                   </div>
@@ -188,20 +196,23 @@ function showCartProducts(array) {
         <hr class="mb-4">
         
         `
-        document.getElementById("cart-items").innerHTML = htmlContentToAppend;
-
-        //Aca es donde entiendo que debera de poder calcularse el subtotal con el control de divisa 
-        //modificando el valor anterior al momento de cargar la pagina
-        //pero se me genera un error que no me doy cuenta cual es
-        
-        ////* calcSubTotal(article.unitCost, i); *///
     }
+
+    //Control para que los controles de cantidad se muestren en el estilo correcto desde el comienzo
+    // y se haga el calculo de subtotal y total una sola vez
+    $("#cart-items").html(htmlContent).ready(function(){
+      for (let i = 0; i < array.length; i++) {
+
+        $(`#count${i}`).trigger("change", "not_subtotal");
+      }
+      calcSubTotal();
+    });
 }
 
 //Funcion para mostrar los articulos en el resumen de pedido
 function showSummaryProducts(array) {
   
-  let htmlContentToAppend = "";
+  let htmlContent = "";
 
     for (let i = 0; i < array.length; i++) {
 
@@ -216,7 +227,7 @@ function showSummaryProducts(array) {
           artSubTotal = article.unitCost * article.count;
         }
 
-        htmlContentToAppend += `
+        htmlContent += `
         <li class="list-group-item font-weight-lighter d-flex justify-content-between align-items-center border-0 px-0 py-1">
           <div>${article.name}</div>
           <div class="d-flex align-items-center">
@@ -225,13 +236,41 @@ function showSummaryProducts(array) {
           </div>
         </li>
         `
-        document.getElementById("summaryItems").innerHTML = htmlContentToAppend;
+        document.getElementById("summaryItems").innerHTML = htmlContent;
     }
 }
 
 // Funcion para contar la cantidad de articulos agregados al carrito
 function showProductsCount(array) {
   document.getElementById("productCount").innerText = array.length;
+  document.getElementById("productSummaryCount").innerText = array.length;
+}
+
+//Funcion para mostrar el formulario de pago segun se elija la opcion de banco o tarjeta
+function showPaymentForms() {
+
+  let radios = document.getElementsByName("paymentType");
+  let cardPaymentForm = document.getElementById("bankPaymentForm");
+  let bankPaymentForm = document.getElementById("cardPaymentForm");
+
+  cardPaymentForm.style.display = 'none';
+  bankPaymentForm.style.display = 'none';
+
+  for (let i = 0; i < radios.length; i++) {
+    radios[i].onclick = function () {
+
+      let val = this.value;
+
+      if (val == 'cardsradio') {
+        cardPaymentForm.style.display = 'none';
+        bankPaymentForm.style.display = 'block';
+      }
+      if (val == 'banksradio') {
+        cardPaymentForm.style.display = 'block';
+        bankPaymentForm.style.display = 'none';
+      }
+    }
+  }
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -248,17 +287,17 @@ document.addEventListener("DOMContentLoaded", function(e){
       // Muestro el conteo de productos agregados al carrito
       showProductsCount(cartPoruducts);
 
+      // Muestro los productos del carrito en el resumen de pedido
+      showSummaryProducts(cartPoruducts);
+
       // Muestro la info de los productos agregados al carrito
       showCartProducts(cartPoruducts);
 
       //Muestro los controles de cantidad
       itemCountControls();
 
-      // Muestro los productos del carrito en el resumen de pedido
-      showSummaryProducts(cartPoruducts);
-
-      //Muestro los valores totales del carrito
-      calcTotal();
+      //Muestro el formulario correspondiente al metodo de pago elegido
+      showPaymentForms();
     }
   });
 
