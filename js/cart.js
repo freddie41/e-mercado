2bfc899583aaf274ddcad7381257a1345e48739e
Federico Cuelho Lariau <fcl41@me.com>
2021-11-29 20:57:37 -0300

    Se agrega conteo unitario en resumen de producto

--- js/cart.js
+++ js/cart.js
@@ -80,6 +80,7 @@ function calcSubTotal() {
   }
 
   $("#subtotalCarrito").html(totalCarrito);
+  showProductsSummary();
   calcTotal();
 }
 
@@ -259,8 +260,12 @@ function showProductsSummary() {
     }
 
     htmlContent += `
-        <li id="artSummary${i}"class="list-group-item font-weight-lighter d-flex justify-content-between align-items-center border-0 px-0 py-1">
-          <div>${article.name}</div>
+        <li id="artSummary${i}"class="list-group-item font-weight-normal d-flex justify-content-between align-items-center border-0 px-0 py-1">
+          <div class="d-flex align-items-center">
+            <span class="mr-2">${article.count}</span>
+            <span class="mr-2 small">x</span>
+            ${article.name}
+          </div>
           <div class="d-flex align-items-center">
             <div class="small mr-1">USD</div>
             <div id="artSummarySubTotal${i}">${artSubTotal}</div>
@@ -604,7 +609,7 @@ $(document).ready(function () {
       html: 'Tu orden ha sido creada.<br>En breves recibirás un email con los datos de la orden.',
       icon: 'success',
       confirmButtonText: 'Aceptar',
-    })
+    });
   }
 })
 //Bindeo de evento que restablece metodo de pago elegido al hacer clic en btn cerrar.
